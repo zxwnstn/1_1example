2d49221ef8b85c98752951d81f02d33e5e72104c
zxwnstn@naver.com <zxwnstn@naver.com>
2018-12-03 22:42:31 +0900

    따베씨 연습

---
+++ 0_1examlpe_Hellow_world/0_1ex.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+	cout << "Hellow, world" << "Hellow, My friend" << "Hellow, god" << endl;
+
+	cout << " using namespace std !!!!" << endl;
+
+	return 0;
+}
