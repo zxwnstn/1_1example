75503225630531df422ad8c7df8e46e9404418ce
zxwnstn@naver.com <zxwnstn@naver.com>
2018-12-04 20:10:56 +0900

    네이밍 연습

---
+++ 1_6exercise_Naming/1_6ex.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//변수명이 불가한 예 찾기
+	int total = 0;
+	int _orange = 0;
+	int VALUE = 0;
+	//int my valriable name = 0; 변수명에 띄어쓰기는 들어갈수없음
+	int Totalcustomer = 0;
+	//int void = 0; 변수명이 예약어와 겹침
+	int n_fruit = 0;
+	//int 2some = 0; 변수명의 시작이 숫자일수 없음
+	int so2me = 0;
+	int meter_of_pipe = 0;
+
+	cout << total << _orange << VALUE << Totalcustomer << n_fruit << so2me << meter_of_pipe << endl;
+
+	return 0;
+}
