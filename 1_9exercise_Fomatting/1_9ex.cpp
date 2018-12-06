33ac13f3c3d0a120b06a12ebde8ebb9f3f2e48d6
zxwnstn@naver.com <zxwnstn@naver.com>
2018-12-06 15:14:26 +0900

    오타 수정

--- 1_9exercise_Fomatting/1_9ex.cpp
+++ 1_9exercise_Fomatting/1_9ex.cpp
@@ -1,6 +1,6 @@
 /*기본적인 서식
 
-1. 여백은 너많이 주지말도록 하자!
+1. 여백은 너무 많이 주지말도록 하자!
 
 2. 줄바꿈 할때 사소한것을 신경쓰자
 ex) cout << "sdfsd" << "sdfsdf" << "sdlfjiejflsejfl" <<
@@ -12,9 +12,9 @@ ex) cout << "sdfsd" << "sdfsdf" << "sdlfjiejflsejfl" <<
 }
 
 4.변수명의 길이가 다를때는
-    int sdjf =      1
-	int sdlfjsj =	3
-	int dfj =		2
+	int sdjf =      1
+	int sdlfjsj =   3
+	int dfj =       2
 	int lsdjfksjf = 4
 등으로 줄맞춤 할수있음
 
