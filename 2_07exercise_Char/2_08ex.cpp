3551461fd41a7293879f7a440b009f5b152672d0
김기섭 <kiseop91@naver.com>
2018-12-11 16:27:16 +0900

    메뉴화면 수정 (이쁘게 ㅎ)

--- 2_07exercise_Char/2_08ex.cpp
+++ 2_07exercise_Char/2_08ex.cpp
@@ -7,24 +7,29 @@ int Ascii(char a)
 }
 
 int main()
-{
-	cout << " 문자를 입력받아 Ascii 숫자로 반환합니다.\n 0을 누른다면 \n\t 종료 됩니다.^^\n";
+{          ///김기섭 메뉴수정함.
+	cout << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" << endl;
+	cout << "■                                                                            ■" << endl;
+	cout << "■ 문자를 입력받아 Ascii 숫자로 반환합니다.   0을  누른다면   종료 됩니다.^^  ■\n";
+	cout << "■                                                                            ■" << endl;
+	cout << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" << endl;
 	while (1)
 	{
-		char a;
-		cout << "문자를 입력하시오" << endl;
-		cin >> a;
-
-		if (a == '0')
+		char ascii;
+		cout << "문자를 입력하시오 : ";
+		cin >> ascii;
+		cout << endl;
+		cout << "※※※" << endl;
+		if (ascii == '0')
 		{
-			cout << '\a' << endl;
+			cout << "※" << '\a' << "※" << endl;
 			break;
 		}
-		
-		else
-			cout << Ascii(a) << endl;
 
-		
+		else
+			cout << Ascii(ascii) << endl;
+
+		cout << "※※※" << endl;
 	}
 	return 0;
 }
