0ebf7e8321446c22761ced939053dd887381c2d9
zxwnstn@naver.com <zxwnstn@naver.com>
2018-12-13 21:12:31 +0900

    ㅇㅅㅇ

--- 3_8excercise_BitwiseOR/3_08ex.cpp
+++ 3_8excercise_BitwiseOR/3_08ex.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
+#include<bitset>
 using namespace std;
 
 int main()
-{
+{	
+	unsigned int a, b;
+	a = 5;
+	b = 12;
+
+	cout << std::bitset<4>(0b0110 >> 2) << endl;
+	cout << "5는 " << std::bitset<4>(a) << "12는 " << std::bitset<4>(b) << endl;
+	cout << "5 & 12는 " << std::bitset<4>(a & b) << endl;
+	cout << "5 | 12는 " << std::bitset<4>(a | b) << endl;
+	cout << "5 ^ 12는 " << std::bitset<4>(a ^ b) << endl;
 
 	return 0;
 }
